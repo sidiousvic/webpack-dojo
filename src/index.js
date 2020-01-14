77b3b476139633f9d8d99325f78a40f6d261da92
sidiousvic <victor6rene@gmail.com>
2020-01-14 16:49:10 +0900

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+import _ from "lodash";
+import "./style.css";
+import dramaPotatoFile from "./dramapotato.png";
+import Data from "./data.xml";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hey", "webpack"], " ");
+  element.classList.add("hello");
 
+  const dramaPotatoImage = new Image();
+  dramaPotatoImage.src = dramaPotatoFile;
+  element.appendChild(dramaPotatoImage);
+
+  console.log(Data);
   return element;
 }
 
